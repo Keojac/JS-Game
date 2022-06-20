325fa45a130f39660dcace78c29074f9bc82e8a1
Kevin Jacob <kevinjacob@kevins-mbp.lan>
2022-06-20 17:09:03 +1000

    created a function to add an image to a div on click

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const container= document.querySelector(".container")
 const grid= document.querySelector(".grid")
+const singleGrids= document.querySelectorAll(".grid")
 
 
 // const humanToken= document.querySelector(".humans")
 // const toggleHuman= () => {
 //     humanToken.classList.toggle(".humans")
 // }
+
+
+
+
+
+
+singleGrids[0].addEventListener("click", (e)=> {
+    const humanImg= document.createElement("img");
+    humanImg.classList.add("humans");
+    humanImg.src="https://img.freepik.com/free-psd/young-businessman-3d-cartoon-avatar-portrait_627936-22.jpg?t=st=1655699814~exp=1655700414~hmac=9399959a481d90b096474bf69e70626be0565a898b79db40a48be4da1c669aab&w=826";
+    e.target.appendChild(humanImg)
+     })
+    
+
 
 
 
@@ -16,7 +31,7 @@ let chooseSquare= singleGrids[randomSquare]
 return chooseSquare
 }
 
-console.log(compMove());
+// console.log(compMove());
 
 
 
