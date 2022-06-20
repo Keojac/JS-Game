cb560808c07ed4494e9dc56d2902017999a4ebc8
Kevin Jacob <kevinjacob@kevins-mbp.lan>
2022-06-20 20:37:41 +1000

    edited tokens and updated img source

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i=0; i < singleGrids.length; i++){
 singleGrids[i].addEventListener("click", (e)=> {
     const humanImg= document.createElement("img");
     humanImg.classList.add("humans");
-    humanImg.src="https://img.freepik.com/free-psd/young-businessman-3d-cartoon-avatar-portrait_627936-22.jpg?t=st=1655699814~exp=1655700414~hmac=9399959a481d90b096474bf69e70626be0565a898b79db40a48be4da1c669aab&w=826";
+    humanImg.src="human token.png";
     e.target.appendChild(humanImg)
     }) 
  }
@@ -20,7 +20,7 @@ let randomSquare= Math.floor(Math.random() * 9)
 let chooseSquare= singleGrids[randomSquare]
 const alienImg= document.createElement("img");
     alienImg.classList.add("aliens");
-    alienImg.src="https://img.freepik.com/free-vector/modern-ufo-abduction-concept-with-flat-design_23-2147912193.jpg?t=st=1655719046~exp=1655719646~hmac=6b0f71fc9d5f590d6697ac64192751309c399d428a32e6e99dd3825d5d53d2ab&w=826";
+    alienImg.src="alien token.png";
     chooseSquare.appendChild(alienImg)
 }
 
