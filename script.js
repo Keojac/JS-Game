2fcc422a6aa17edc1fbf622e17b703cf2be48a10
Kevin Jacob <kevinjacob@kevins-mbp.lan>
2022-06-20 16:03:06 +1000

    computer move function bit written

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const container= document.querySelector(".container")
 const grid= document.querySelector(".grid")
-container.append(grid)
+
+
+// const humanToken= document.querySelector(".humans")
+// const toggleHuman= () => {
+//     humanToken.classList.toggle(".humans")
+// }
+
+
+
+const compMove= () => {
+const singleGrids= document.querySelectorAll(".grid")
+let randomSquare= Math.floor(Math.random() * 9)
+let chooseSquare= singleGrids[randomSquare]
+return chooseSquare
+}
+
+console.log(compMove());
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const makeSquare = () => {
