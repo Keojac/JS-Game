491ef20671f03e0e656f8ff73d5ae0fe248614f1
Kevin Jacob <kevinjacob@kevins-mbp.lan>
2022-06-20 14:09:17 +1000

    created background, h1 and container

---
+++ README.md
@@ -0,0 +1,38 @@
+# Heading 1
+
+lorem 
+
+## Heading 2
+
+**Bold text** 
+_Italics_
+
+> Block quotes
+
+Run this command: `git add.`
+
+## JS Sample
+```js
+// Full code block
+const bar= foo
+
+```
+
+## CSS Sample
+``` css
+heading {
+    font-size: 24px;
+}
+```
+
+# Links
+- [Google](https://google.com)
+- [Bing](https://bing.com)
+
+1. Number 1
+1. Number 2
+1. Number 3
+
+![Alt Text= "doggo"](https://placedog.net/500)
+
+[My Stylesheet](./style.css)
---
+++ script.js
@@ -0,0 +1,21 @@
+const container= document.querySelector(".container")
+
+
+// const makeSquare = () => {
+//     const div= document.createElement("div")
+//     div.classList.add("square")
+//     container.appendChild(div)
+// }
+
+// const creategrid = (squares) => {
+//     for (let i=1; i< squares; i++){
+//         console.log(i);
+//         makeSquare();
+//     }
+// }
+
+// creategrid(9)
+
+// const div= document.createElement("div")
+//     div.classList.add("square")
+//     container.appendChild(div)
