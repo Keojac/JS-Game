9e6f5f3cf5dd145a024d2eb95e94e382b4f77746
Kevin Jacob <kevinjacob@kevins-mbp.lan>
2022-06-24 10:01:18 +1000

    edited some things and finished readme

--- README.md
+++ README.md
@@ -18,3 +18,47 @@ This is my first project completed for the Software Engineering Immersive bootca
 
 ---
 
+### Creative Process
+
+I started off focussing on the CSS elements such as creating the grid and background so it would be easier for me to visualise the direction I was heading with this project. I decided on a theme of space and a battle between humans and aliens to give more of a story for my user. Creating a kind of high stakes where the hope of humanity centred on the user's choices. 
+
+![Alt text= "Gameboard Design"](./Screen%20Shot%202022-06-23%20at%203.31.51%20pm.png)
+
+![Alt text= "Gameboard Design"](./Screen%20Shot%202022-06-23%20at%203.32.25%20pm.png)
+
+--- 
+
+### Game Mechanisms
+
+The game starts as soon as you click any square and places a human token with the subsequent alien token being places in one of the other squares.
+
+The aim of the game is to get three pictures in a row whether that be horizontally, vertically or diagonally.
+
+The scoreboard to the left and right of the screen indicates the total number of wins for the humans and aliens in the current session. 
+
+When the game comes to an end through a win or draw, a window will pop up indicating the personalised win/ draw outcome with the ability to reset the game with the button. (As seen in the image above)
+
+--- 
+
+### Debugging Process
+
+In the making of the game I encountered a number of bugs and errors. One of the main errors involved the human win score counting for three and the alien win score counting for two even though both scores were set to one. Through some teamwork and collaboration we were able to pinpoint the bug being towards lines of code which initially created a condition where the checkWin function was set to true. This condition created multiple times in the javascript file meant that the checkWin function was run multiple times, causing the score evaluation to multiply. This was fixed by created a new variable and using that as the condition as opposed to the previous method.
+
+--- 
+
+### Installation
+
+To play this game, download this repository and open the index.html file in a web browser.
+
+---
+
+### References
+
+Background, end game screens and human and alien tokens were pictures taken from [Free Pik](www.freepik.com)
+
+- Background Image: https://www.freepik.com/free-vector/falling-stars-beautiful-night-background_5376553.htm#query=space%20background&position=20&from_view=search
+- Draw Screen Image: https://www.freepik.com/free-photo/soldier-guarding-design_879705.htm#query=soldiers&position=19&from_view=search 
+- Human Win Screen Image: https://www.freepik.com/free-vector/flat-people-dancing-illustration_17721643.htm#query=party&position=16&from_view=search#position=16&query=party
+- Alien Win Screen Image: https://www.freepik.com/free-vector/evil-alien-robot-machine-attacking-people-city-cartoon_15080142.htm#query=alien%20robot%20machine%20attacking&position=1&from_view=search
+- Human Token Image: https://www.freepik.com/free-psd/young-businessman-3d-cartoon-avatar-portrait_25842057.htm#query=smiling%20person&position=23&from_view=keyword
+- Alien Token Image: https://www.freepik.com/free-vector/modern-ufo-abduction-concept-with-flat-design_2889277.htm#query=alien&position=9&from_view=search 
--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const container= document.querySelector(".container")
-const singleGrids= document.querySelectorAll(".grid")
-const gameOver= false
-const hWinScreen= document.getElementById("hwin")
-const aWinScreen= document.getElementById("awin")
-const dScreen= document.getElementById("draw")
-const hScore= document.getElementById("hscore")
-let hCount= document.getElementById("hpoints")
-let aCount= document.getElementById("apoints")
-let hgameScore=0;
-let agameScore=0;
-let isWinner= false;
+const container = document.querySelector(".container")
+const singleGrids = document.querySelectorAll(".grid")
+const gameOver = false;
+const hWinScreen = document.getElementById("hwin")
+const aWinScreen = document.getElementById("awin")
+const dScreen = document.getElementById("draw")
+const hScore = document.getElementById("hscore")
 
-let emptyDivs= []
-let picDivs= []
+let hCount = document.getElementById("hpoints")
+let aCount = document.getElementById("apoints")
+let hgameScore = 0;
+let agameScore = 0;
+let isWinner = false;
+
+let emptyDivs = [];
+let picDivs = [];
 
 
-const compMove= () => {
+const compMove = () => {
     if (isWinner === true){
         return 
     } else if (picDivs.length === 9) {
         return
     } else { 
-let randomSquare= Math.floor(Math.random() * emptyDivs.length)
-let chooseSquare= emptyDivs[randomSquare]
-const alienImg= document.createElement("img");
+    let randomSquare = Math.floor(Math.random() * emptyDivs.length);
+    let chooseSquare = emptyDivs[randomSquare];
+    const alienImg = document.createElement("img");
     alienImg.classList.add("aliens");
     alienImg.setAttribute("id", "ufo")
     alienImg.src="alien-token.png";
@@ -36,130 +37,125 @@ const alienImg= document.createElement("img");
 }
 
 
-const humMove= () => {
+const humMove = () => {
 
-    for (let i=0; i < singleGrids.length; i++){
+    for (let i = 0; i < singleGrids.length; i++){
     singleGrids[i].addEventListener("click", (e)=> {
         if (isWinner === true) {
             return
         }
         else if (picDivs.length === 9){
             return 
-        } else {   
-        const humanImg= document.createElement("img");
-        humanImg.classList.add("humans");
-        humanImg.setAttribute("id", "sapien")
-        humanImg.src="human-token.png";
-        e.target.appendChild(humanImg);
-        emptyDivs.push(e.target)
-        checkEmptyDiv();
-        checkPicDiv();
-        checkWin();
-        checkDraw();
-        compMove();
+        } else if (singleGrids[i].childNodes.length !== 1) {   
+            const humanImg= document.createElement("img");
+            humanImg.classList.add("humans");
+            humanImg.setAttribute("id", "sapien")
+            humanImg.src="human-token.png";
+            e.target.appendChild(humanImg);
+            emptyDivs.push(e.target)
+            checkEmptyDiv();
+            checkPicDiv();
+            checkWin();
+            checkDraw();
+            compMove();
+        } else {
+            return
         }
-        }) 
-     } 
-    }
-    humMove();
+    }) 
+  } 
+}
+
+humMove();
+
 
 const checkEmptyDiv = () => {
-    emptyDivs= [];
-    for (let i=0; i < singleGrids.length; i++){
+    emptyDivs = [];
+    for (let i = 0; i < singleGrids.length; i++){
         if (singleGrids[i].childNodes.length === 0){
             emptyDivs.push(singleGrids[i])      
         }
-} 
+    } 
 }
 
+
 const checkPicDiv = () => {
-    picDivs= [];
-    for (let i=0; i < singleGrids.length; i++){
+    picDivs = [];
+    for (let i = 0; i < singleGrids.length; i++){
         if (singleGrids[i].childNodes.length === 1){
             picDivs.push(singleGrids[i])
             
         }
-} console.log(picDivs);
+    } console.log(picDivs);
 }
 
-const winConditions = [[singleGrids[0], singleGrids[1], singleGrids[2]], [singleGrids[0], singleGrids[3], singleGrids[6]], [singleGrids[0], singleGrids[4], singleGrids[8]], 
+
+const winConditions = [
+[singleGrids[0], singleGrids[1], singleGrids[2]], 
+[singleGrids[0], singleGrids[3], singleGrids[6]], 
+[singleGrids[0], singleGrids[4], singleGrids[8]], 
 [singleGrids[1], singleGrids[4], singleGrids[7]], 
-[singleGrids[2], singleGrids[5], singleGrids[8]], [singleGrids[2], singleGrids[4], singleGrids[6]],
+[singleGrids[2], singleGrids[5], singleGrids[8]], 
+[singleGrids[2], singleGrids[4], singleGrids[6]],
 [singleGrids[3], singleGrids[4], singleGrids[5]],
-[singleGrids[6], singleGrids[7], singleGrids[8]]]
+[singleGrids[6], singleGrids[7], singleGrids[8]]
+]
+
 
 const checkWin= () => {
-    for (let i=0; i<winConditions.length; i++){
+    for (let i = 0; i<winConditions.length; i++){
         if(winConditions[i][0].childNodes.length !== 0 && winConditions[i][1].childNodes.length !== 0 && winConditions[i][2].childNodes.length !== 0){
-        const a= winConditions[i][0].childNodes[0].getAttribute("src")
-        const b= winConditions[i][1].childNodes[0].getAttribute("src")
-        const c= winConditions[i][2].childNodes[0].getAttribute("src")
+            const a = winConditions[i][0].childNodes[0].getAttribute("src")
+            const b = winConditions[i][1].childNodes[0].getAttribute("src")
+            const c = winConditions[i][2].childNodes[0].getAttribute("src")
         if (a === b && b === c && a === "human-token.png"){
                 console.log("Humans win!");
                 hWinScreen.classList.add("show");
                 hgameScore += 1
-                hCount.textContent= hgameScore
-                isWinner= true
-             } else if (a === b && b === c && a === "alien-token.png"){
+                hCount.textContent = hgameScore;
+                isWinner = true;
+        } else if (a === b && b === c && a === "alien-token.png"){
                 console.log("Aliens win!");
                 aWinScreen.classList.add("ashow");
-                agameScore += 1
-                aCount.textContent= agameScore
-                isWinner= true
-           }
-        //      if (a === "human-token.png"){
-        //         console.log("Humans win!");
-        //         hWinScreen.classList.add("show");
-        //         let gameScore= 0;
-        //         gameScore += 1
-        //         hCount.textContent= gameScore
-        //         return true
-        //      } else if (a === "alien-token.png"){
-        //         console.log("Aliens win!");
-        //         aWinScreen.classList.add("ashow");
-        //         let gameScore= 0;
-        //         gameScore += 1
-        //         aCount.textContent= gameScore
-        //         return true
-        //    }
+                agameScore += 1;
+                aCount.textContent= agameScore;
+                isWinner = true;
+            }
            }
         }
         return false
-        }
+    }
     
 
 
-const checkDraw= () => {
+const checkDraw = () => {
     if (isWinner === true) {
         return
-    }
-    else if (picDivs.length === 9 && isWinner === true) {
+    } else if (picDivs.length === 9 && isWinner === true) {
         return
-    }
-    else if (picDivs.length === 9 && isWinner === false) {
+    } else if (picDivs.length === 9 && isWinner === false) {
         console.log("It's a draw!");
         dScreen.classList.add("dshow");
         return
     }
 }      
     
-const restartGame= () => {
+
+const restartGame = () => {
     hWinScreen.classList.remove("show")
     aWinScreen.classList.remove("ashow")
     dScreen.classList.remove("dshow")
     for (let i = 0; i < singleGrids.length; i++) {
-    singleGrids[i].innerHTML=''
+    singleGrids[i].innerHTML = ''
     }
     checkEmptyDiv();
     checkPicDiv();
-    isWinner= false
+    isWinner = false;
 }
+
 
 document.getElementById("hrestartbutton").addEventListener("click", restartGame)
 document.getElementById("arestartbutton").addEventListener("click", restartGame)
 document.getElementById("drestartbutton").addEventListener("click", restartGame)
-
-
 
 
 // This is for a 9x9 grid if I have time
